fa1f47236172560d274229e05fd71bcaea0b2c36
poemlove-123 <912567741@qq.com>
2025-05-16 06:44:11 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,86 @@
+﻿define({ entries : {
+    "badreddine2020logic": {
+        "author": "Badreddine, Selim and Garcez, Artur d'Avila and Serafini, Luciano and Spranger, Michael",
+        "journal": "arXiv preprint arXiv:2012.13635",
+        "title": "Logic Tensor Networks",
+        "type": "article",
+        "url": "https://doi.org/10.48550/arXiv.2012.13635",
+        "year": "2020"
+    },
+    "genesereth2003logic": {
+        "author": "Genesereth, Michael R. and Nilsson, Nils J.",
+        "howpublished": "\\url{https://plato.stanford.edu/entries/logic-ai/}",
+        "note": "Stanford Encyclopedia of Philosophy",
+        "title": "Logic-Based Artificial Intelligence",
+        "type": "misc",
+        "year": "2003"
+    },
+    "goranko2010tableau": {
+        "author": "Goranko, Valentin",
+        "howpublished": "\\url{https://www2.imm.dtu.dk/courses/02286/Slides/FirstOrderLogicSemanticTableauTrans.pdf}",
+        "note": "DTU Informatics Lecture Notes",
+        "title": "First-order semantic tableau",
+        "type": "misc",
+        "year": "2010"
+    },
+    "laurent2010game": {
+        "author": "Laurent, Olivier",
+        "howpublished": "\\url{https://perso.ens-lyon.fr/olivier.laurent/folgames.pdf}",
+        "note": "ENS Lyon Lecture Notes",
+        "title": "Game semantics for first-order logic",
+        "type": "misc",
+        "year": "2010"
+    },
+    "leitsch2004automated": {
+        "author": "Leitsch, Alexander",
+        "howpublished": "\\url{https://www.logic.at/people/leitsch/mbbook.pdf",
+        "publisher": "Logic.at",
+        "title": "Automated Model Building",
+        "type": "book",
+        "year": "2004"
+    },
+    "lund2000game": {
+        "author": "Lund University",
+        "howpublished": "\\url{https://fileadmin.cs.lth.se/ai/psfiles/GameTheoreticalSemantics.pdf}",
+        "note": "Lecture Material",
+        "title": "Game-theoretical semantics for first-order logic",
+        "type": "misc",
+        "year": "2000"
+    },
+    "springer1990automated": {
+        "author": "Sprenger, H. and Nutt, W.",
+        "doi": "10.1007/BF02022100",
+        "journal": "Annals of Operations Research",
+        "number": "1",
+        "pages": "105--120",
+        "publisher": "Springer",
+        "title": "Automated model construction: A logic-based approach",
+        "type": "article",
+        "volume": "27",
+        "year": "1990"
+    },
+    "tum2019tableaux": {
+        "author": "TUM Logic Group",
+        "howpublished": "\\url{https://www21.in.tum.de/teaching/sar/SS20/2.pdf}",
+        "note": "Technical University of Munich Lecture Notes",
+        "title": "Propositional and First-order Logic Tableaux: Concept and Heuristics",
+        "type": "misc",
+        "year": "2019"
+    },
+    "vanbenthem2001logic": {
+        "author": "van Benthem, Johan",
+        "howpublished": "\\url{https://staff.science.uva.nl/~johan/GL",
+        "note": "University of Amsterdam Preprint",
+        "title": "Logic Games are Complete for Game Logics",
+        "type": "misc",
+        "year": "2001"
+    },
+    "wikipedia2024game": {
+        "author": "{Wikipedia contributors}",
+        "howpublished": "\\url{https://en.wikipedia.org/wiki/Game_semantics}",
+        "note": "Wikipedia Article",
+        "title": "Game semantics",
+        "type": "misc",
+        "year": "2024"
+    }
+}});
